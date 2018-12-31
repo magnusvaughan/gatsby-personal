abf4e929daf646d38d846e21df8de5e6ba2d7ed6
mvaughan <magnus.vaughan@gmail.com>
2018-12-31 22:54:12 +0000

    Changed font and legend

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `I'm a software developer`,
+    title: `I'm a software developer - cool huh?`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `grey`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
